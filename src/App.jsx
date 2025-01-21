904edb2e6cc3080fbb32f767a26dd129da892466
DeyvisBenito <deyvisbenito@gmail.com>
2025-01-21 13:40:28 -0600

    Portafolio Deyvis Saul Benito Medrano

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,10 @@ import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import { PortafolioPage } from "./pages/PortafolioPage";
 import { ProyectosPage } from "./pages/ProyectosPage";
 import { ContactarPage } from "./pages/ContactarPage";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import 'react-toastify/dist/ReactToastify.css';
 import './styles/custom.css'
 
 export const App = () => {
